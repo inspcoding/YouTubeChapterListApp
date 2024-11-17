0ce072a43e6ba57beed33a15292a53941b81c272
Nick <nick@inspcoding.com>
2024-11-16 21:27:13 -0600

    Made sure the timecode format was correct for YouTube and changed the header color.

--- YouTubeChapterList/Program.cs
+++ YouTubeChapterList/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using YouTubeChapterList;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents();
+builder.Services.AddHttpClient();
 
 await builder.Build().RunAsync();
